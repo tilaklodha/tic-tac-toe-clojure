945aa164d04d5a1fd514fe5af02fd7bcaa04abc8
tilaklodha <tilaklodha@gmail.com>
2018-05-27 16:07:39 +0530

    Add dependency for clojure coverage

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-cloverage "1.0.11-SNAPSHOT"]]
   :main ^:skip-aot tic-tac-toe.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
